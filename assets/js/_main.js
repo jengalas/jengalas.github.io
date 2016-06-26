93610361784c287f4daf3940f99a0a91612cbc60
Jennifer Galas <jennifer.galas@scranton.edu>
2016-06-26 12:47:51 -0400

    Add super-search

--- assets/js/_main.js
+++ assets/js/_main.js
@@ -74,4 +74,12 @@ $(document).ready(function(){
     closeOnContentClick: true,
     midClick: true // allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source.
   });
+
+  superSearch({
+    searchFile: '//jengalas.github.io/feed.xml',
+    searchSelector: '#js-super-search', // CSS Selector for search container element.
+    inputSelector: '#js-super-search__input', // CSS selector for <input>
+    resultsSelector: '#js-super-search__results' // CSS selector for results container
+  });
+
 });
